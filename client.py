db442c5d9e7da491e45ba300747af4fb167d904f
tcsb16 <tcsb16@mailinator.com>
2018-05-26 13:09:23 -0700

    init commit

--- client.py
+++ client.py
@@ -12,15 +12,51 @@ included functionality tests.
 from base_client import BaseClient, IntegrityError
 from crypto import CryptoError
 
+def path_join(*strings):
+    """Joins a list of strings putting a "/" between each.
+
+    :param strings: a list of strings to join
+    :returns: a string
+    """
+    return '/'.join(strings)
 
 class Client(BaseClient):
     def __init__(self, storage_server, public_key_server, crypto_object,
                  username):
         super().__init__(storage_server, public_key_server, crypto_object,
                          username)
+        location = username + "my key"
 
+        self.key = self.storage_server.get(location)
+
+        if not key:
+            self.key = self.crypto.get_random_bytes(32)
+
+
+            encrypted_key = self.crypto.asymmetric_encrypt(self.key, self.pks.get_encryption_key(self.username))
+            encrypted_key += self.crypto. asymmetric_sign(encrypted_key, self.rsa_priv_key)
+
+
+            self.storage_server.put(location, encrypted_key)
+        else:
+            self.key = self.crypto.asymmetric_decrypt(self.key, self.elg_priv_key)            
+            if self.crypto.asymmetric_verify(encrypted_key, , self.pks.get_encryption_key(self.username))
+
+    def resolve(self, uid):
+        while True:
+            res = self.storage_server.get(uid)
+            if res is None or res.startswith("[DATA]"):
+                return uid
+            elif res.startswith("[POINTER]"):
+                uid = res[10:]
+            else:
+                raise IntegrityError()
     def upload(self, name, value):
-        # Replace with your implementation
+        
+        uid = self.resolve(path_join(self.username, name))
+
+        self.storage_server.put(uid, "[DATA] " + value)
+
         raise NotImplementedError
 
     def download(self, name):
@@ -37,4 +73,10 @@ class Client(BaseClient):
 
     def revoke(self, user, name):
         # Replace with your implementation (not needed for Part 1)
-        raise NotImplementedError
+        raise NotImplementedErrdef path_join(*strings):
+    """Joins a list of strings putting a "/" between each.
+
+    :param strings: a list of strings to join
+    :returns: a string
+    """
+    return '/'.join(strings)or
